90f7c99833bc0f89f35918e432f0edd5c4119c88
Tetrabiter <noa.eskenkot@gmail.com>
2025-02-17 12:43:29 -0500

    add routing in frontebd

---
+++ frontend/app/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+const ForgotPassword = () => {
+    return ( 
+        <div>
+            <p>Password</p>
+        </div> 
+    );
+}
+ 
+export default ForgotPassword;
---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const NotFound = () => {
+    return ( 
+        <div>
+            <p>Not-found page</p>
+            <Link href="/"></Link>
+        </div>
+    );
+}
+ 
+export default NotFound;
